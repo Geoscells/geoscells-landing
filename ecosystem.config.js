cde2d52c00ff2a2f86617e7d9e3865d1d85ff0d3
Alexey Sapon <asapon@freeconferencecall.com>
2024-02-04 02:25:42 +0200

    Added ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
-const ENV_PATH = '/etc/frontend/.env';
+const ENV_PATH = '/etc/landing/.env';
 
 module.exports = {
   apps: [
     {
       name: 'landing',
-      script: 'serve',
+      script: 'start',
       env: {
-        PM2_SERVE_PATH: './build',
-        PM2_SERVE_PORT: 8080,
-        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_PORT: 8081,
         PM2_HOME: '/var/www'
       }
     }
@@ -18,9 +16,9 @@ module.exports = {
       user: 'alexiuscrow',
       host: '10.156.0.3',
       ref: 'origin/main',
-      repo: 'git@frontend_deploy:alexiuscrow/geoscells-landing.git',
-      path: '/home/frontend',
-      'post-deploy': `npm install --legacy-peer-deps && env-cmd -f ${ENV_PATH} npm run build && PM2_HOME=/var/www pm2 reload ecosystem.config.js --env production`,
+      repo: 'git@landing_deploy:alexiuscrow/geoscells-landing.git',
+      path: '/home/landing',
+      'post-deploy': `npm install && env-cmd -f ${ENV_PATH} npm run build && PM2_HOME=/var/www pm2 reload ecosystem.config.js --env production`,
       key: '~/.ssh/geoscells'
     }
   }
