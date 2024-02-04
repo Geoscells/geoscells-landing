9d872189914d8723e9d04a72455ced116e328f75
Alexey Sapon <asapon@freeconferencecall.com>
2024-02-04 03:10:19 +0200

    Updated pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
   apps: [
     {
       name: 'landing',
-      script: 'start',
+      script: 'npm start',
       env: {
         PM2_SERVE_PORT: 8081,
         PM2_HOME: '/var/www'
