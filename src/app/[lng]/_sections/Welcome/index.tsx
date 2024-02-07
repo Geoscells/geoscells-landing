deedc43ebb3e4b1975a8e7b27212625d2d899c24
Alexey Sapon <asapon@freeconferencecall.com>
2024-02-08 00:47:49 +0200

    Updated highlight in the welcome section

--- src/app/[lng]/_sections/Welcome/index.tsx
+++ src/app/[lng]/_sections/Welcome/index.tsx
@@ -18,25 +18,6 @@ const Welcome: SectionCommonType = async ({lng}) => {
           <div className={style.title}>
             {t('sections.welcome.title.main')}
             <span className={style.highlightedSuffix}>{t('sections.welcome.title.highlightedSuffix')}</span>
-            <svg
-              style={{visibility: 'hidden', position: 'absolute'}}
-              width="0"
-              height="0"
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1">
-              <defs>
-                <filter id="goo">
-                  <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
-                  <feColorMatrix
-                    in="blur"
-                    mode="matrix"
-                    values="1 0 0 0 0  0 0.274 0 0 0  0 0 0.447 0 0  0 0 0 19 -9"
-                    result="goo"
-                  />
-                  <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-                </filter>
-              </defs>
-            </svg>
           </div>
           <ul className={style.points}>{pointsJsx}</ul>
           <div className={style.learnMoreTitle}>{t('sections.welcome.learnMoreTitle')}</div>
